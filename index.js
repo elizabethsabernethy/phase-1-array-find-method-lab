0bf0f453b5599061663b7019b0badd7940cfa241
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-11-28 11:32:34 -0500

    commit tree 5e37e702af59bf63d5f1b734a753c614e675d934

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+    function win(results){
+        return (results.result === 'W');
+    }
+   if (record.find(win) !== undefined){
+    return (record.find(win)).year;
+   }
+   else{
+    return undefined;
+   }
+}
+
